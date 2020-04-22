5c031211bec4a79b653cc58432f72b52d8eae23e
Alexey <01002@mail.ru>
2020-04-22 12:01:00 +0300

    moy commit 1

---
+++ script.js
@@ -0,0 +1,51 @@
+// КОММЕНТАРИЙ
+
+'use strict';
+// var leftBorderWidth = 1;
+//  {
+//     let second = 2;
+// }
+// let second = 2;
+// const pi = 3.14;
+
+
+// console.log(leftBorderWidth);
+// console.log(second);
+// console.log(4/0);
+// console.log('string'/0);
+
+
+// let persone = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+// }
+
+// console.log(persone["name"]);
+
+
+let arr =['plum.png', 'orange.jpg', 'apple.bmp'];
+ console.log(arr[0])
+
+
+// const obj ={
+//     name: 'Alex'
+// }
+// const arr =[1,3,5,6];
+
+
+// to String
+// console.log("WW"+"FFF");
+
+// // в числа
+
+// console.log(typeof(Number('5')));
+
+// console.log(typeof(+'5'));
+
+// console.log(typeof(parseInt('15PX', 10)));
+
+// alert('hello');
+
+let answer = prompt('еСТЬ ЛИ 18?', 'ДА');
+console.log(typeof(arr));
